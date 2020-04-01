1cc7331c86e58127d07e68519577e2554626af21
Federico <maggiorefederico@outlook.com>
2020-04-01 13:43:53 +0200

    fixing winning stage

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
 function computerPlay () {
     let numb = Math.random()* 3;
-    console.log(numb)
-    let res = "";
     if (numb <= 1) return "rock";
     if (numb <=2) return "paper";
     else return "scissors";
@@ -15,7 +13,12 @@ function game (playerMove, PCmove) {
     return winner;
 }
 
+function printWinner (winnerName) {
+    p1.innerHTML = "We finally have a winner!"
+    p2.innerHTML = `Congrats ${winnerName} for winning this AMAZING game!`;
+}
 
+let warWinner = "";
 let playerPoints = 0;
 let PCpoints = 0;
 let buttons = document.getElementsByClassName('button');
@@ -26,38 +29,37 @@ let reset = document.getElementById("reset");
 reset.onclick = function () {
     playerPoints = 0;
     PCpoints = 0;
+    warWinner = "";
     score.innerHTML = "0 : 0";
     p1.innerHTML = "";
     p2.innerHTML = "";
 }
 
 for (let i = 0; i < buttons.length; i++){
-    let button = buttons[i];
+    console.log(i)
+    let button = buttons[i];  
     button.onclick = function () {
-        if (playerPoints < 5 && PCpoints < 5){
+        if (playerPoints < 5 && PCpoints  < 5){
             let playerMove = button.id;
             let PCmove = computerPlay();
             let winner = game(playerMove, PCmove);
-            if (winner == "player") playerPoints++;
-            if (winner == "pc") PCpoints++;
+
+            if (winner == "player") {
+                playerPoints++;
+            }
+            if (winner == "pc") {
+                PCpoints++;
+            }
             score.innerHTML = ""+ playerPoints + " : " + PCpoints;
-            p1.innerHTML = "The human chose " + playerMove + ", the computer chose " + PCmove;
+            p1.innerHTML = "The human picked " + playerMove + ", whereas the computer chose " + PCmove;
             if (winner!= "tie") p2.innerHTML = "The winner is: " + winner;
-            else p2.innerHTML = "this was a tie!";
-            
+            else p2.innerHTML = "this battle was a tie!";   
         }
         else {
-            if (playerPoints == 5) {
-                p1.innerHTML = "The player is the winner!";
-                p2.innerHTML = "";
-            }
-            else {
-                p1.innerHTML = "The PC is the winner!";
-                p2.innerHTML = "";
-            }
+            if(PCpoints ==5) printWinner("PC");
+            else printWinner("USER");
         }
     }
-    
-
-}
+}  
+console.log("xxxxxxxxx");
 
